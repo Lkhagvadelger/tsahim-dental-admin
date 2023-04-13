ed6831dfbeac6253c7f59499a3d69eec054ba038
Marka <92774748+lkhagvadelgermh@users.noreply.github.com>
2023-04-14 01:53:00 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import { HeroSection } from "@lib/landingpage/ui/herosection";
-import {
-  Box, NavMenuLayout,
-  SEO
-} from "@ui/index";
+import { Box, NavMenuLayout, SEO } from "@ui/index";
 import useTranslation from "next-translate/useTranslation";
 
 const LandingPage = () => {
   const { t } = useTranslation("app");
   return (
     <Box w="full">
-      <SEO title={t("landing-title")} />
+      <SEO title={"Нүүр"} />
       <NavMenuLayout>
         <HeroSection />
       </NavMenuLayout>
